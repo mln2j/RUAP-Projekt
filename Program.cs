669b69c129e80048c51e328c87d0ff7d0781aefb
Mihael Lendvaj <mlendvaj@Mihaels-MacBook-Pro.local>
2024-11-05 12:05:33 +0100

    Update helloWorld, add my jmbag

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Pozdrav iz aplikacije od Mihaela Lendvaja");
+Console.WriteLine("Pozdrav iz aplikacije od Mihaela Lendvaja [JMBAG: 0165081992]");
